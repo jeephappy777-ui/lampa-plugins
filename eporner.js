df7e6a406bb18f2227d1ea23caa01441c1750bc3
jeephappy777-ui <jeephappy777@gmail.com>
2026-04-26 18:54:01 +0200

    Update eporner.js

--- eporner.js
+++ eporner.js
@@ -1,80 +1,57 @@
 (function () {
     'use strict';
 
-    function redtubePlugin(object) {
+    function eporner(object) {
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({mask: true, over: true});
         var body    = $('<div class="category-full"></div>');
         
-        // Используем стандартный прокси Lampa
-        var cors = 'https://cors.lampa.mx/';
+        // Используем более надежный прокси для обхода "Сеть недоступна"
+        var proxy = 'https://api.allorigins.win/raw?url=';
 
         this.create = function () {
             var _this = this;
             this.activity.loader(true);
+            var url = object.url || 'https://www.eporner.com/api/v2/video/search/?per_page=40&order=top-weekly';
             
-            // API RedTube
-            var url = object.url || 'https://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&size=40';
-            
-            network.silent(cors + url, function (data) {
-                if (data && data.videos && data.videos.length > 0) {
-                    _this.build(data.videos);
-                } else {
-                    _this.empty();
-                }
+            network.silent(proxy + encodeURIComponent(url), function (data) {
+                try {
+                    var json = typeof data === 'string' ? JSON.parse(data) : data;
+                    if (json && json.videos) _this.build(json.videos);
+                    else _this.empty();
+                } catch(e) { _this.empty(); }
             }, function () {
                 _this.empty();
             });
-
             return this.render();
         };
 
         this.build = function (videos) {
             var _this = this;
-            body.empty();
-            videos.forEach(function (obj) {
-                var video = obj.video; // У RedTube данные вложены в объект video
-                var item = Lampa.Template.get('card', {
-                    title: video.title,
-                    release_year: video.duration
-                });
-
+            videos.forEach(function (video) {
+                var item = Lampa.Template.get('card', { title: video.title, release_year: video.length_min + ' min' });
                 item.addClass('card--collection');
-                
-                // Картинка
-                var img = item.find('.card__img')[0];
-                if (img) img.src = 'https://images.weserv.nl/?url=' + encodeURIComponent(video.default_thumb);
-
+                item.find('.card__img').attr('src', 'https://images.weserv.nl/?url=' + encodeURIComponent(video.default_thumb.src));
                 item.on('hover:enter', function () {
-                    Lampa.Player.play({
-                        url: video.url, // Плеер подхватит ссылку
-                        title: video.title
-                    });
+                    Lampa.Player.play({ url: video.embed, title: video.title });
                 });
-
                 body.append(item);
             });
-
             this.activity.loader(false);
             this.activity.toggle();
         };
 
         this.empty = function () {
             this.activity.loader(false);
-            body.append('<div class="empty">Контент не найден. Попробуйте другую категорию.</div>');
+            body.append('<div class="empty">Контент не найден. Проверьте VPN.</div>');
             this.activity.toggle();
         };
 
-        this.render = function () {
-            scroll.append(body);
-            return scroll.render();
-        };
+        this.render = function () { scroll.append(body); return scroll.render(); };
 
         this.start = function () {
             Lampa.Controller.add('content', {
-                toggle: function () {
-                    Lampa.Controller.collectionSet(body);
-                },
+                toggle: function () { Lampa.Controller.collectionSet(body); },
                 left: function () { Lampa.Controller.toggle('menu'); },
                 up: function () { Lampa.Controller.toggle('head'); },
                 back: function () { Lampa.Activity.backward(); }
@@ -84,80 +61,50 @@
 
         this.pause = function () {};
         this.stop = function () {};
-        this.destroy = function () {
-            network.clear();
-            scroll.destroy();
-            body.remove();
-        };
+        this.destroy = function () { network.clear(); scroll.destroy(); body.remove(); };
     }
 
-    function redtubeEntry() {
+    function eporner_cats() {
         var scroll = new Lampa.Scroll({mask: true, over: true});
         var body   = $('<div class="category-full"></div>');
-
         this.create = function () {
-            var cats = [
-                {t: 'Популярные', q: 'stars'},
-                {t: 'Новинки', q: 'newest'},
-                {t: 'Гейша', q: 'Japanese'},
-                {t: 'Аматоры', q: 'Amateur'},
-                {t: 'Мильфы', q: 'MILF'},
-                {t: 'Подростки', q: 'Teen'}
-            ];
-
-            cats.forEach(function (item) {
+            var genres = [{t:'Популярные', q:'top-weekly'}, {t:'Новинки', q:'latest'}, {t:'Teen', q:'teen'}];
+            genres.forEach(function (item) {
                 var card = Lampa.Template.get('card', { title: item.t, release_year: '' });
                 card.addClass('card--category');
-                card.find('.card__img').attr('src', 'https://openclipart.org/download/218529/video-play-icon.svg');
-                
-                var url = 'https://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&size=40&category=' + item.q;
-
                 card.on('hover:enter', function () {
                     Lampa.Activity.push({
                         title: item.t,
-                        url: url,
-                        component: 'redtube_plugin'
+                        url: 'https://www.eporner.com/api/v2/video/search/?per_page=40&query=' + item.q,
+                        component: 'eporner_mod'
                     });
                 });
                 body.append(card);
             });
-
             return this.render();
         };
-
         this.start = function () {
-            Lampa.Controller.add('content', {
-                toggle: function () { Lampa.Controller.collectionSet(body); },
-                back: function () { Lampa.Activity.backward(); }
-            });
+            Lampa.Controller.add('content', { toggle: function () { Lampa.Controller.collectionSet(body); }, back: function () { Lampa.Activity.backward(); } });
             Lampa.Controller.toggle('content');
         };
-
         this.render = function () { scroll.append(body); return scroll.render(); };
         this.destroy = function () { scroll.destroy(); body.remove(); };
     }
 
-    function startPlugin() {
-        Lampa.Component.add('redtube_plugin', redtubePlugin);
-        Lampa.Component.add('redtube_entry', redtubeEntry);
+    // Регистрация
+    Lampa.Component.add('eporner_mod', eporner);
+    Lampa.Component.add('eporner_cats', eporner_cats);
 
-        var add = function() {
-            if ($('div[data-action="redtube"]').length > 0) return;
-            var item = $('<div class="menu__item selector" data-action="redtube">' +
-                '<div class="menu__ico"><svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28 1.9.07.8.1 1.49.1 2.09L22 12c0 .6-.03 1.29-.1 2.09-.06.8-.15 1.43-.28 1.9-.13.47-.35.83-.66 1.07-.31.24-.7.39-1.17.45-.47.06-1.1.11-1.9.15-.8.04-1.49.06-2.09.06L12 18c-.6 0-1.29-.02-2.09-.06-.8-.04-1.43-.09-1.9-.15-.47-.06-.86-.21-1.17-.45-.31-.24-.53-.6-.66-1.07-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L6 12c0-.6.03-1.29.1-2.09.06-.8.15-1.43.28-1.9.13-.47.35-.83.66-1.07.31-.24.7-.39 1.17-.45.47-.06 1.1-.11 1.9-.15.8-.04 1.49-.06 2.09-.06L12 6c.6 0 1.29.02 2.09.06.8.04 1.43.09 1.9.15.47.06.86.21 1.17.45.31.24.53.6.66 1.07z" fill="currentColor"/></svg></div>' +
-                '<div class="menu__text">RedTube</div>' +
-            '</div>');
-
-            item.on('hover:enter', function () {
-                Lampa.Activity.push({ title: 'RedTube', component: 'redtube_entry' });
-            });
-
-            $('.menu .menu__list').first().append(item);
-        };
-
-        if (window.appready) add();
-        else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') add(); });
+    function addMenu() {
+        var item = $('<div class="menu__item selector" data-action="eporner">' +
+            '<div class="menu__text">EPORNER Pro</div>' +
+        '</div>');
+        item.on('hover:enter', function () {
+            Lampa.Activity.push({ title: 'EPORNER Pro', component: 'eporner_cats' });
+        });
+        $('.menu .menu__list').first().append(item);
     }
 
-    startPlugin();
+    if (window.appready) addMenu();
+    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') addMenu(); });
 })();
