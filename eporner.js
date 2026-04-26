eb67f50b96930ddabe0d1040daeeed7c68408547
jeephappy777-ui <jeephappy777@gmail.com>
2026-04-26 18:39:29 +0200

    Update eporner.js

--- eporner.js
+++ eporner.js
@@ -5,11 +5,8 @@
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({mask: true, over: true, check_bottom: true});
         var items   = [];
-        var html    = $('<div></div>');
         var body    = $('<div class="category-full"></div>');
-        
-        // Используем их проверенный прокси
-        var cors = 'https://cors.lampa.mx/';
+        var cors    = 'https://cors.lampa.mx/';
 
         this.create = function () {
             var _this = this;
@@ -32,16 +29,14 @@
 
         this.build = function (data) {
             var _this = this;
-            
             data.videos.forEach(function (video) {
                 var item = Lampa.Template.get('card', {
                     title: video.title,
                     release_year: video.length_min + ' min'
                 });
-
                 item.addClass('card--collection');
                 
-                // Картинки проксируем точно так же, как в рабочих плагинах
+                // Проксирование картинок через weserv
                 item.find('.card__img').attr('src', 'https://images.weserv.nl/?url=' + encodeURIComponent(video.default_thumb.src));
 
                 item.on('hover:enter', function () {
@@ -50,18 +45,16 @@
                         title: video.title
                     });
                 });
-
                 body.append(item);
                 items.push(item);
             });
-
             this.activity.loader(false);
             this.activity.toggle();
         };
 
         this.empty = function () {
             this.activity.loader(false);
-            body.append('<div class="empty">Контент не найден. Проверьте VPN или прокси.</div>');
+            body.append('<div class="empty">Контент не найден. Проверьте VPN.</div>');
             this.activity.toggle();
         };
 
@@ -74,7 +67,6 @@
             Lampa.Controller.add('content', {
                 toggle: function () {
                     Lampa.Controller.collectionSet(body);
-                    Lampa.Controller.follow('container');
                 },
                 left: function () { Lampa.Controller.toggle('menu'); },
                 up: function () { Lampa.Controller.toggle('head'); },
@@ -88,7 +80,6 @@
         this.destroy = function () {
             network.clear();
             scroll.destroy();
-            items.forEach(function (item) { item.removeAllListeners(); });
             body.remove();
         };
     }
@@ -98,16 +89,11 @@
         var body   = $('<div class="category-full"></div>');
 
         this.create = function () {
-            // Весь список категорий, которые ты просил
             var genres = [
-                {t: 'Популярные', q: 'top-weekly'}, {t: 'Новинки', q: 'latest'}, {t: 'Топ дня', q: 'top-daily'},
+                {t: 'Популярные', q: 'top-weekly'}, {t: 'Новинки', q: 'latest'}, 
                 {t: 'Teen', q: 'teen'}, {t: 'Milf', q: 'milf'}, {t: 'Amateur', q: 'amateur'},
-                {t: 'Anal', q: 'anal'}, {t: 'Big Tits', q: 'big-tits'}, {t: 'Ebony', q: 'ebony'},
-                {t: 'Japanese', q: 'japanese'}, {t: 'Hardcore', q: 'hardcore'}, {t: 'Reality', q: 'reality'},
-                {t: 'Solo', q: 'solo'}, {t: 'Vintage', q: 'vintage'}, {t: 'POV', q: 'pov'},
-                {t: 'BDSM', q: 'bdsm'}, {t: 'Massage', q: 'massage'}, {t: 'Public', q: 'public'}
+                {t: 'Anal', q: 'anal'}, {t: 'Big Tits', q: 'big-tits'}, {t: 'Ebony', q: 'ebony'}
             ];
-
             genres.forEach(function (item) {
                 var card = Lampa.Template.get('card', { title: item.t, release_year: '' });
                 card.addClass('card--category');
@@ -126,7 +112,6 @@
                 });
                 body.append(card);
             });
-
             return this.render();
         };
 
@@ -134,7 +119,6 @@
             Lampa.Controller.add('content', {
                 toggle: function () {
                     Lampa.Controller.collectionSet(body);
-                    Lampa.Controller.follow('container');
                 },
                 left: function () { Lampa.Controller.toggle('menu'); },
                 up: function () { Lampa.Controller.toggle('head'); },
@@ -163,14 +147,9 @@
                 '<div class="menu__ico"><svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="currentColor"/></svg></div>' +
                 '<div class="menu__text">EPORNER Pro</div>' +
             '</div>');
-
             item.on('hover:enter', function () {
-                Lampa.Activity.push({
-                    title: 'EPORNER Pro',
-                    component: 'eporner_categories'
-                });
+                Lampa.Activity.push({ title: 'EPORNER Pro', component: 'eporner_categories' });
             });
-
             $('.menu .menu__list').first().append(item);
         }
 
